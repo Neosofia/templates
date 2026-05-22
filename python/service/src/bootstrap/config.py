bd05885e29da0bcc06dd1b3d22fadc264983689f
Ben Young <Ben@neosofia.tech>
2026-05-22 11:18:35 -0400

    feat: remove iss claim validation; bump sdk to v0.9.0

--- python/service/src/bootstrap/config.py
+++ python/service/src/bootstrap/config.py
@@ -24,7 +24,6 @@ class Settings(BaseSettings):
     # JWT authentication settings
     jwt_public_key: str | None = Field(default=None)
     jwt_jwks_uri: str | None = Field(default=None)
-    jwt_issuer: str = Field(default="https://neosofia.com")
     jwt_audience: str | list[str] = Field(default="python-template")
 
     # Rate limit settings
